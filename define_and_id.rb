cc79b39abebe9193a1d08fe91356e6ba5cf38e83
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-01-26 12:33:05 -0700

    Initial commit

---
+++ define_and_id.rb
@@ -0,0 +1,23 @@
+# Run each line of code below (either from this file or in IRB individually).
+#  Then, in a ruby comment, write 1-2 sentences describing what is happening, using ALL the involved vocabulary terms you've learned in this lesson so far.
+
+# EXAMPLE
+# The upcase method is called on the string object "Hello World"
+# No arguments are passed; upcase has one clear job which is to capitalize all letters that exist in the String
+# The return value is "HI"
+"Hello World".upcase
+
+
+"Hello World".include?("Hello")
+
+
+"Hello World".end_with?("Hello")
+
+
+"Hello World".end_with?("rld")
+
+
+"Hello World".length
+
+
+"Hello World".size
