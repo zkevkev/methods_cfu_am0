2e9f8624e8a011082bc335fd102010ef6f9fd47c
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-01-26 13:09:13 -0700

    Add practice

---
+++ final_practice.rb
@@ -0,0 +1,38 @@
+# Defining Our Own Methods
+# 1: Write a method named greeting that prints out a general greeting to someone
+
+# What is the return value of your method?
+# How many arguments did you pass your method?
+
+
+
+
+#2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+
+
+# What is the return value of your method?
+# How many arguments did you pass your method?
+# What data type was your argument(s)?
+
+
+
+
+#3: Write a method named square that takes in one number, and returns the square of that number
+
+
+
+# What is the return value of your method?
+# How many arguments did you pass your method?
+# What data type was your argument(s)?
+
+
+
+
+#4: Write a method named full name that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
+
+
+
+# What is the return value of your method?
+# How many arguments did you pass your method?
+# What data type was your argument(s)?
+
---
+++ methods_variables.rb
@@ -0,0 +1,32 @@
+# =================================
+# PART 1
+
+# Define a variable that stores a string
+
+#  call upcase on the variable, print it out
+#  call downcase on the variable, print it out
+#  call reverse on the variable, print it out
+#  call lenght on the variable, print it out
+
+
+
+
+
+
+
+# =================================
+# PART 2
+
+user_name = "coco_11am"
+last_login = "12/09/2021"
+
+# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
+  # Write the methods out on the lines below, with an explanation in your own words of how they work.
+
+
+
+# 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
+
+
+
+
