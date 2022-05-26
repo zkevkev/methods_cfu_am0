63cdc221b9c17e8761346c7e9a81c4fc511bd9eb
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-05-26 10:04:08 -0500

    Update define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -2,10 +2,12 @@
 #  Then, in a ruby comment, write 1-2 sentences describing what is happening, using ALL the involved vocabulary terms you've learned in this lesson so far.
 
 # EXAMPLE
-# The upcase method is called on the string object "Hello World"
-# No arguments are passed; upcase has one clear job which is to capitalize all letters that exist in the String
-# The return value is "HI"
-"Hello World".upcase
+# The downcase method is called on the string object "Hello World"
+# No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
+# The return value is "hello world"
+"Hello World".downcase
+
+# YOU DO the rest:
 
 
 "Hello World".include?("Hello")
@@ -17,7 +19,9 @@
 "Hello World".end_with?("rld")
 
 
-"Hello World".length
+12.even?
 
 
-"Hello World".size
+18.next
+
+
