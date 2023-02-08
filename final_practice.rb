4758d48ac6a8b9b2e5dac17dd75bbbe71791fb96
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-02-08 17:42:52 -0600

    Add another challenge

--- final_practice.rb
+++ final_practice.rb
@@ -25,9 +25,24 @@
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
+# Bonus: Print a sentence that interpolates the return value of your square method.
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
-# Bonus: Print a sentence that interpolates the return value of your square method.
+
+# 5: Write a method named check_stock that satisfies the following interaction pattern:
+# Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+check_stock(4, "Coffee");
+# => "Coffee is stocked"
+
+check_stock(3, "Tortillas");
+# => "Tortillas - running LOW"
+
+check_stock(0, "Cheese");
+# => "Cheese - OUT of stock!"
+
+check_stock(1, "Salsa");
+# => "Salsa - running LOW"
